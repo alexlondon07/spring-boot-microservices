d9b11ad8faa45a072dde934ffa2ba82be82c0f79
GLOBANT\alexander.londono <alexander.londono@globant.com>
2021-10-13 22:20:42 -0500

    EUREKA ITEMS

--- items-service/items-service/src/main/java/api/microservices/itemsservice/ItemsServiceApplication.java
+++ items-service/items-service/src/main/java/api/microservices/itemsservice/ItemsServiceApplication.java
@@ -2,10 +2,11 @@ package api.microservices.itemsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name ="service-product")
+@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 public class ItemsServiceApplication {
