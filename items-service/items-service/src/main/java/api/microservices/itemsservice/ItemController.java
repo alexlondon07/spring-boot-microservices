7d39ea65c26a4b2109f4fbcea41e18e20040d5d9
Alexander Andrés Londoño Espejo <alexlondon07@gmail.com>
2021-12-07 16:40:39 -0500

    52. La anotación @TimeLimiter

--- items-service/items-service/src/main/java/api/microservices/itemsservice/ItemController.java
+++ items-service/items-service/src/main/java/api/microservices/itemsservice/ItemController.java
@@ -4,6 +4,7 @@ import api.microservices.itemsservice.model.Item;
 import api.microservices.itemsservice.model.Product;
 import api.microservices.itemsservice.service.ItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 
 @RestController
@@ -48,10 +50,23 @@ public class ItemController {
         return itemService.findById(id, quantity);
     }
 
+    @CircuitBreaker(name="items", fallbackMethod = "alternativeMethod2")
+    @TimeLimiter(name="items", fallbackMethod = "alternativeMethod2")
+    @GetMapping("/view3/{id}/quantity/{quantity}")
+    public CompletableFuture<Item> getDetail3(@PathVariable Long id, @PathVariable Integer quantity){
+        return CompletableFuture.supplyAsync( ()-> itemService.findById(id, quantity)) ;
+    }
+
     public Item alternativeMethod(Long id, Integer quantity, Throwable e) {
         logger.info(e.getMessage());
         Product product = Product.builder().id(id).name("Camera Sony").price(500.00).build();
         return Item.builder().quantity(quantity).product(product).build();
+    }
+
+    public CompletableFuture<Item>  alternativeMethod2(Long id, Integer quantity, Throwable e) {
+        logger.info(e.getMessage());
+        Product product = Product.builder().id(id).name("Camera Sony v2").price(800.00).build();
+        return CompletableFuture.supplyAsync( ()->Item.builder().quantity(quantity).product(product).build() ) ;
     }
 }
 
