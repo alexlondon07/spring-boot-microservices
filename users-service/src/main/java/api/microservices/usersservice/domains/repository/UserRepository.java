e747802e209f2f901ffca7cdfcb086c3c1fc7537
Alexander Andrés Londoño Espejo <alexlondon07@gmail.com>
2022-01-13 12:18:24 -0500

    RestResource

--- users-service/src/main/java/api/microservices/usersservice/domains/repository/UserRepository.java
+++ users-service/src/main/java/api/microservices/usersservice/domains/repository/UserRepository.java
@@ -3,13 +3,15 @@ package api.microservices.usersservice.domains.repository;
 import api.microservices.usersservice.domains.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
-    public User findByUsername(String username);
+    @RestResource(path = "search-username")
+    public User findByUsername(@Param("username") String username);
 
     @Query("select u from User u where u.username =?1")
     public User findByUsername2(String username);
